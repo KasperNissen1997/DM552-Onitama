26256f6e8913d6be18f8371eea029ca7586e2859
KasperNissen1997 <KasperNissen1997@gmail.com>
2022-03-19 13:43:52 +0100

    Initialized template project

---
+++ src/Lib.hs
@@ -0,0 +1,18 @@
+module Lib
+  ( generateGame
+  , simulateGame
+  , countGames
+  ) where
+
+import System.IO
+import Control.Monad
+import System.Random
+
+simulateGame :: FilePath -> IO String
+simulateGame _ = undefined
+
+generateGame :: Integer -> Integer -> String
+generateGame _ _ = undefined
+
+countGames :: Integer -> FilePath -> IO String
+countGames _ _ = undefined
